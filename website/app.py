9f3ef2f3e69c8851bc1bdd33f0f714caab0f2faf
Rafael Dias <rafaeldsd93@gmail.com>
2023-04-17 18:45:47 +0100

    some updates in website

---
+++ website/app.py
@@ -0,0 +1,17 @@
+import random
+import time
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+# Google Maps API key (replace with your own)
+#GOOGLE_MAPS_API_KEY ="AIzaSyDcLG_2KgktdQJXLaeyQZHJzmvcSjNwoPM"
+
+@app.route("/")
+def home():
+    # Render home page with Google Maps and location data
+    return render_template("home.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
