102d25f2ddcf6abd15f26f0d0939e9791e358204
rafaeldsd <rafaeldsd93@gmail.com>
2023-05-12 19:19:02 +0100

    little details

--- coordinates.py
+++ coordinates.py
@@ -3,15 +3,16 @@ import json
 import datetime
 from polyline import decode 
 
-gmaps = googlemaps.Client(key='AIzaSyB5IcIzPvGKxfBjbzl6x1f_T_DdABVk-u4')
 
-directions_result = gmaps.directions((40.630519, -8.654069) , (40.633434, -8.658704), mode="driving", departure_time=datetime.datetime.now())
-coords_list = []
+def get_coords(lat1, lon1, lat2, lon2):   
+    gmaps = googlemaps.Client(key='AIzaSyB5IcIzPvGKxfBjbzl6x1f_T_DdABVk-u4')
 
-for step in directions_result[0]['legs'][0]['steps']:
-    decoded_points = decode(step['polyline']['points'])
-    for lat, lng in decoded_points:
-        coords_list.append({"latitude": lat, "longitude": lng})
-# write it in the static/coords.json file
-with open('static/police_coords.json', 'w') as file:
-    json.dump(coords_list, file)
+    directions_result = gmaps.directions((lat1, lon1),(lat2, lon2),mode="driving",departure_time=datetime.datetime.now())
+    coords_list = []
+
+    for step in directions_result[0]['legs'][0]['steps']:
+        decoded_points = decode(step['polyline']['points'])
+        for lat, lng in decoded_points:
+            coords_list.append({"latitude": lat, "longitude": lng})
+
+    return coords_list
